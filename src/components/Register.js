3315bfbbd7726d5d8ff44ceb57be3d55aeeb03a7
Danesh Tolani <danesh.tolani@gmail.com>
2022-12-02 23:42:50 +0530

    removed login utils

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { registerUser } from "../utils/handleLogin";
 import { useNavigate } from "react-router-dom";
 import "./Register.css";
 // * appwrite
